18519eb84fb8a7b4bc8df2d299e716648e06f0c9
Aastik <aastikdude7@gmail.com>
2024-01-05 23:03:49 +0530

    word replacement function

---
+++ word-replace.py
@@ -0,0 +1,6 @@
+def replace_word():
+
+    str = "Hi guys, I am Aastik, and hi"
+    word_to_replace = input("Enter the word to replace: ")
+    word_replacement = input("Enter the word replacement: ")
+    
